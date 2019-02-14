8a8d016bea316c3db64aa0c0dda02aa676907618
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2019-02-14 03:56:05 +0100

    BWD-321 forgot add files

---
+++ js_events/forms.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from aldryn_categories.models import Category
+from aldryn_people.models import Person
+from js_services.models import Service
+from .cms_appconfig import EventsConfig
+
+
+LAYOUT_CHOICES = [
+    ('cols', 'Columns'),
+    ('rows', 'Rows'),
+    ('hero', 'Hero'),
+    ('events', 'Events'),
+]
+
+TIME_PERIODS = [
+    ('all', 'All'),
+    ('future', 'Future'),
+    ('past', 'Past'),
+]
+
+
+class EventRelatedPluginForm(forms.ModelForm):
+
+    layout = forms.ChoiceField(LAYOUT_CHOICES)
+    time_period = forms.ChoiceField(TIME_PERIODS)
+
+    related_types = forms.ModelMultipleChoiceField(
+        queryset=EventsConfig.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple('Related sections', False)
+    )
+    related_categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple('Related categories', False)
+    )
+    related_services = forms.ModelMultipleChoiceField(
+        queryset=Service.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple('Related services', False)
+    )
+    related_hosts = forms.ModelMultipleChoiceField(
+        queryset=Person.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple('Related hosts', False)
+    )
